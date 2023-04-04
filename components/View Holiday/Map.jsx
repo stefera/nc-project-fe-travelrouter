b5fe97f97e9165bd026074d98c57ea2e60fe0f86
mollybrace <molly.rose.brace@gmail.com>
2023-04-04 18:12:09 +0700

    adding debugged map

--- components/View Holiday/Map.jsx
+++ components/View Holiday/Map.jsx
@@ -1,35 +1,38 @@
-import React from ‘react’;
-import MapView, {Marker} from ‘react-native-maps’;
-import { StyleSheet, View } from ‘react-native’;
-import { useEffect, useState } from ‘react’;
-import { Polyline } from ‘react-native-maps’;
-import MapViewDirections from ‘react-native-maps-directions’;
+import React from "react";
+import MapView, {Marker} from "react-native-maps";
+import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Polyline } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions"
+
 const Map = () => {
+
       const tripData = [
         {
-          title: “trip1”,
-          author: “fergus”,
+          title: "trip1",
+          author: "fergus",
           startLocation: {
-            city: “nottingham”,
+            city: "nottingham",
             coordinates: {              latitude: 53.954,              longitude: -1.4            },
           },
           destination: {
-            city: “manchester”,
+            city: "manchester",
             coordinates: {              latitude: 50.5,              longitude: -4            },
-            arrivalDate: “2023-04-04”,
-            departureDate: “2023-04-08",
+            arrivalDate: "2023-04-04",
+            departureDate: "2023-04-08",
             activities: [
               {
-                name: “The Warehouse Project”,
-                address: “Mayfield Train Station, The Depot, Manchester M1 2QF”,
+                name: "The Warehouse Project",
+                address: "Mayfield Train Station, The Depot, Manchester M1 2QF",
                 coordinates: {
                   latitude: 53.4756,
                   longitude: -2.2253,
                 },
               },
+
               {
-                name: “Hidden at Downtex Mill”,
-                address: “Mayfield Train Station, The Depot, Manchester M1 2QF”,
+                name: "Hidden at Downtex Mill",
+                address: "Mayfield Train Station, The Depot, Manchester M1 2QF", 
                 coordinates: {
                   latitude: 53.4756,
                   longitude: -2.2253,
@@ -39,36 +42,39 @@ const Map = () => {
           },
         },
       ];
+
   return (
     <MapView
-    style={{ height: “50%” }}
-    provider=“google”
+    style={{ height: "50%" }}
+    provider="google"
     >
-      {tripData.map((item) => {
+      {tripData.map((item, index) => {
          return (
            <Marker
-             key=“test”
+             key={index}
              coordinate={item.startLocation.coordinates}
-             title=“Test marker start location”
+             title="Test marker start location"
              />
          )
       })}
-       {tripData.map((item) => {
+      
+       {tripData.map((item, index) => {
          return (
            <Marker
-             key=“test”
+             key={index}
              coordinate={item.destination.coordinates}
-             title=“Test marker end location”
+             title="Test marker end location"
              />
          )
       })}
+
 {tripData[0].destination.activities.map((activity, index) => {
           return (
           <Marker
             key={index}
             coordinate={activity.coordinates}
             title={activity.name}
-            image={require(“../../assets/beachflag.png”)}
+            image={require("../../assets/beachflag.png")}
             /> 
             )
          })}
@@ -76,8 +82,9 @@ const Map = () => {
   origin={{latitude: 53.954, longitude: -1.4}}
   destination={{latitude: 50.5, longitude: -4}}
   strokeWidth={3}
-  strokeColor=“red”
+  strokeColor="#FF0000"
 />
+
 </MapView>
   );
 }
