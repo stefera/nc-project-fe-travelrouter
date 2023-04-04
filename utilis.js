ce5e4035bc4b82327bd421eb5e8e0ed9997df640
FergusJames212 <fergusarjames@virginmedia.com>
2023-04-04 14:46:28 +0100

    fix geocoding fail

--- utilis.js
+++ utilis.js
@@ -21,9 +21,10 @@ export const fetchActivites = (params) => {
 };
 
 export const geoCodeLocations = (params) => {
-  console.log("here in utils", params);
+  const cities = params;
+  console.log (cities);
   return db
-    .get("/api/trips/geoCodeActivities", { params: params })
+    .get("/api/trips/geoCodeActivities", { params: cities })
     .then(({ data }) => {
       console.log(data.geoCodedLocations, "final cities");
       return data.geoCodedLocations;
